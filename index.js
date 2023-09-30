0f07eec011d514a37c7accd2afc81ffc1bc244f9
NavneetYadav19 <86159340+NavneetYadav19@users.noreply.github.com>
2023-09-30 11:44:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-let string = "";
-let buttons = document.querySelectorAll(".button");
-Array.from(buttons).forEach((button) => {
-  button.addEventListener("click", (e) => {
-    if (e.target.innerHTML == "=") {
-      string = eval(string);
-      document.querySelector("input").value = string;
-    }
-    else if (e.target.innerHTML == "C") {
-      string = ""
-      document.querySelector("input").value = string;
-    }
-     else {
-      console.log(e.target);
-      string = string + e.target.innerHTML;
-      document.querySelector("input").value = string;
-    }
-  });
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "="];
+let output = "";
+
+const calculate = (btnValue) => {
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
