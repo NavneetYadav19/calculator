92bf6be8701e0fd5a198d2a83096624507466015
NavneetYadav19 <86159340+NavneetYadav19@users.noreply.github.com>
2023-09-30 09:40:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ Array.from(buttons).forEach((button) => {
     if (e.target.innerHTML == "=") {
       string = eval(string);
       document.querySelector("input").value = string;
-    } else {
+    }
+    else if (e.target.innerHTML == "C") {
+      string = ""
+      document.querySelector("input").value = string;
+    }
+     else {
       console.log(e.target);
       string = string + e.target.innerHTML;
       document.querySelector("input").value = string;
